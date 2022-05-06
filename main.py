7b44b7219c2467ae3a53b4ca40b0474ff421ecbe
jsayram <2413200+jsayram@users.noreply.github.com>
2022-05-06 00:44:30 -0400

    reading pokemon names from api test

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import requests
+import json
+
+pokemon_name = {}
+for id in range(1,4):
+   # print(f'id:{id}')
+    response_API = requests.get(f'https://pokeapi.co/api/v2/pokemon/{id}/')
+    #print(response_API.status_code)
+    data = response_API.text
+    parsed_data = json.loads(data)
+    pokemon_name[id] = parsed_data['name']
+    
+
+print(pokemon_name.get(3))
+
+
+
+# for location in parsed_data['Andaman and Nicobar Islands']['districtData']['South Andaman']:
+#     print(location)
+
+# active_case = parsed_data['Andaman and Nicobar Islands']['districtData']['South Andaman']['active']
+
+#print("Active cases in South Andaman:", active_case)
+
