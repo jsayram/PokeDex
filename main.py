0635108dfc5b5eaaa9c48ad03e7b20ac9b1aea1d
jsayram <2413200+jsayram@users.noreply.github.com>
2022-05-06 21:18:38 -0400

    added flask server scaffolding with html and temporary routes

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import requests
-import json
+#run to start webserver or website
+from website import create_app   #the reason we can do this is because website is a python package
+# import requests
+# import json
+# from flask import Flask
 
-pokemon_name = {}
-for id in range(1,4):
-   # print(f'id:{id}')
-    response_API = requests.get(f'https://pokeapi.co/api/v2/pokemon/{id}/')
-    #print(response_API.status_code)
-    data = response_API.text
-    parsed_data = json.loads(data)
-    pokemon_name[id] = parsed_data['name']
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+
+
+
+
+
+
+# pokemon_name = {}
+# for id in range(1,4):
+#    # print(f'id:{id}')
+#     response_API = requests.get(f'https://pokeapi.co/api/v2/pokemon/{id}/')
+#     #print(response_API.status_code)
+#     data = response_API.text
+#     parsed_data = json.loads(data)
+#     pokemon_name[id] = parsed_data['name']
     
 
-print(pokemon_name.get(3))
-
-
-
-# for location in parsed_data['Andaman and Nicobar Islands']['districtData']['South Andaman']:
-#     print(location)
-
-# active_case = parsed_data['Andaman and Nicobar Islands']['districtData']['South Andaman']['active']
-
-#print("Active cases in South Andaman:", active_case)
-
+# print(pokemon_name.get(3))
